6957f5d75cdbbe311152857d95a812f39dcddbb4
Caleb Etemesi <etemesicaleb@gmail.com>
2022-02-14 23:56:23 +0300

    Moved constants from fse_compress to constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,13 @@
 /// Upper bits to be used  for encoding
 /// and decoding
 pub const LIMIT: usize = 11;
+
+pub const SMALL_CHUNK_SIZE: usize = 20;
+
+/// Recommended table
+pub const TABLE_LOG: usize = 10;
+/// Maximum size of table.
+/// If the table goes above this, some invaraints in encoding and decoding won't work
+pub const MAX_TABLE_LOG: usize = 11;
+
+pub const TABLE_SIZE: usize = 1 << TABLE_LOG;
