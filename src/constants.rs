bd0a684985ed26d8243b8d40c0ff742a17603e41
Caleb Etemesi <etemesicaleb@gmail.com>
2022-03-03 09:36:16 +0300

    Changed state generator format

--- src/constants.rs
+++ src/constants.rs
@@ -4,13 +4,15 @@ pub const LIMIT: usize = 11;
 
 pub const SMALL_CHUNK_SIZE: usize = 20;
 
-/// Recommended table
+/// Recommended table  size
 pub const TABLE_LOG: usize = 11;
 /// Maximum size of table.
-/// If the table goes above this, some invaraints in encoding and decoding won't work
+/// If the table goes above this, some invariants in encoding and decoding won't work
 pub const MAX_TABLE_LOG: usize = 11;
 
 pub const TABLE_SIZE: usize = 1 << TABLE_LOG;
+/// Minimum table size
+pub const MIN_TABLE_LOG: usize = 5;
 
 pub fn state_generator(num_states: usize) -> usize
 {
@@ -21,7 +23,7 @@ pub fn state_generator(num_states: usize) -> usize
      *  But for smaller state counts, e.g if we decide to use 9 bits, switch
      *  to the more well distributed state of
      *  of (num_states >> 1) | (num_states>>3) | 3 (fse/xpack)
-     *  or probably use a different distribution hueristic.
+     *  or probably use a different distribution heuristic.
      */
-    return (num_states >> 2) | (num_states >> 3) | 5;
+    return (num_states >> 2) | (num_states >> 3) | 1;
 }
