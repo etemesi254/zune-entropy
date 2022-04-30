a9a68e78e9d08b7f42a13b5c33a1946235db78ae
caleb <etemesicaleb@gmail.com>
2022-04-30 17:28:45 +0300

    huff_decompress: Read from a buffer and not from a read trait

--- src/huff_decompress.rs
+++ src/huff_decompress.rs
@@ -1,6 +1,6 @@
 //! This module provides huffman encoding and decoding routines
 
-use std::io::Read;
+
 
 pub use crate::constants::LIMIT;
 use crate::errors::EntropyErrors;
@@ -303,9 +303,7 @@ fn decode_symbols_fallback(
         | !stream4.check_final()
         | !stream5.check_final()
     {
-        return Err(EntropyErrors::CorruptStream(format!(
-            "A stream  was possibly corrupted, integrity of data is compromised",
-        )));
+        return Err(EntropyErrors::CorruptStream("A stream  was possibly corrupted, integrity of data is compromised".to_string()));
     }
     //everything is good, we won Mr Stark.
     Ok(())
@@ -315,33 +313,28 @@ fn decode_symbols_fallback(
 /// Caveats to understand for dest
 /// we will write from `dest.len()`  going forward, after decompression
 /// is done , dest.len() will contain uncompressed data
-pub fn huff_decompress<R: Read>(src: &mut R, dest: &mut Vec<u8>) -> Result<(), EntropyErrors>
+pub fn huff_decompress(src: &[u8], dest: &mut Vec<u8>) -> Result<(), EntropyErrors>
 {
+    let mut block_length ;
+    let mut symbols:&[u8];
+    let mut jump_table: &[u8; 10];
+
+    let mut tbl = [0; 1 << LIMIT];
+    let mut code_lengths :[u8; 12] = [0;12];
+
+
+    // current position from where we are reading src_buf from.
+    let mut src_position = 4;
+
     // read block information
-    let mut block_info = [0];
+    let mut block_info = src[0];
 
-    src.read_exact(&mut block_info)?;
-
-    let mut length = [0, 0, 0, 0];
     // read the length
-    src.read_exact(&mut length[0..3])?;
-
-    let mut block_length = u32::from_le_bytes(length);
-
-    // not all bytes will be used
-    let mut source = vec![0; block_length as usize];
+    let mut length = [0, 0, 0, 0];
+    length[0..3].copy_from_slice(&src[1..4]);
 
     // we know that blocks have equal sizes except the last one
     // so if we read the first one we can determine how much space we will need
-
-    let mut tbl = [0; 1 << LIMIT];
-
-    let mut jump_table = [0; 10];
-
-    let mut code_lengths = [0; 12];
-
-    let mut symbols = [0; 256];
-
     loop
     {
         block_length = u32::from_le_bytes(length);
@@ -351,16 +344,16 @@ pub fn huff_decompress<R: Read>(src: &mut R, dest: &mut Vec<u8>) -> Result<(), E
             dest.reserve(block_length as usize);
         }
         // 0b10 uncompressed
-        if (block_info[0] >> 6) == 0b10
+        if (block_info >> 6) == 0b10
         {
             read_uncompressed(src, block_length, dest);
         }
-        else if (block_info[0] >> 6) == 0b01
+        else if (block_info >> 6) == 0b01
         {
             // RLE block
             read_rle(src, block_length, dest);
         }
-        else if (block_info[0] >> 6) == 0b11
+        else if (block_info >> 6) == 0b11
         {
             // fse compressed block
             panic!("FSE block encountered where Huffman Was expected");
@@ -370,24 +363,28 @@ pub fn huff_decompress<R: Read>(src: &mut R, dest: &mut Vec<u8>) -> Result<(), E
             // compressed block.
 
             // read jump table
-            src.read_exact(&mut jump_table)?;
+            jump_table = src[src_position..src_position + 10].try_into().unwrap();
+            src_position+=10;
+            //src.read_exact(&mut jump_table)?;
 
             // two bytes per jump table, stored in little endian form.
-            let tbl1 = u32::from(jump_table[0]) + (u32::from(jump_table[1]) << 8);
+            let tbl1 = usize::from(u16::from_le_bytes(jump_table[0..2].try_into().unwrap()));
 
-            let tbl2 = u32::from(jump_table[2]) + (u32::from(jump_table[3]) << 8) + tbl1;
+            let tbl2 = usize::from(u16::from_le_bytes(jump_table[2..4].try_into().unwrap())) + tbl1;
 
-            let tbl3 = u32::from(jump_table[4]) + (u32::from(jump_table[5]) << 8) + tbl2;
+            let tbl3 = usize::from(u16::from_le_bytes(jump_table[4..6].try_into().unwrap())) + tbl2;
 
-            let tbl4 = u32::from(jump_table[6]) + (u32::from(jump_table[7]) << 8) + tbl3;
+            let tbl4 = usize::from(u16::from_le_bytes(jump_table[6..8].try_into().unwrap())) + tbl3;
 
             // end of this stream.
-            let end = u32::from(jump_table[8]) + (u32::from(jump_table[9]) << 8) + tbl4;
+            let end = usize::from(u16::from_le_bytes(jump_table[8..10].try_into().unwrap())) + tbl4;
 
-            let offsets = [tbl1, tbl2, tbl3, tbl4, end].map(|x| x as usize);
+            let offsets = [tbl1, tbl2, tbl3, tbl4, end];
 
             // read code lengths
-            src.read_exact(&mut code_lengths[1..])?;
+            code_lengths[1..].copy_from_slice(&src[src_position..src_position+11]);
+            src_position+=11;
+            //src.read_exact(&mut code_lengths[1..])?;
 
             let codes = code_lengths.iter().map(|x| *x as usize).sum::<usize>();
             if codes > 256
@@ -397,14 +394,14 @@ pub fn huff_decompress<R: Read>(src: &mut R, dest: &mut Vec<u8>) -> Result<(), E
                 ));
             }
             // read symbols
-            src.read_exact(&mut symbols[0..codes])?;
+            symbols = &src[src_position..src_position+codes];
+            src_position+=codes;
 
             // Build the Huffman tree
-            build_tree(&mut tbl, &code_lengths, &symbols)?;
+            build_tree(&mut tbl, &code_lengths, symbols)?;
 
-            let huff_source = &mut source[0..end as usize];
-
-            src.read_exact(huff_source)?;
+            let huff_source =  &src[src_position..src_position+end];
+            src_position+=end;
             // new length
             let start = dest.len();
 
@@ -440,15 +437,19 @@ pub fn huff_decompress<R: Read>(src: &mut R, dest: &mut Vec<u8>) -> Result<(), E
         }
         // top bit in block info indicates if block is the last
         // block.
-        if (block_info[0] >> 5 & 1) == 1
+        if (block_info >> 5 & 1) == 1
         {
             // end of block
             break;
         }
         // not last block, pull in more bytes.
-        src.read_exact(&mut block_info)?;
+        block_info = src[src_position];
+        src_position+=1;
+
         // read the length for the next iteration
-        src.read_exact(&mut length[0..3])?;
+        length[0..3].copy_from_slice(&src[src_position..src_position+3]);
+        src_position+=3;
+        //src.read_exact(&mut length[0..3])?;
     }
     Ok(())
 }
